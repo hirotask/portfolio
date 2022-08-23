275641df993129b320538ed5fcc709c54fd16e3a
hirotask <hirotaskhashi@gmail.com>
2022-08-23 16:16:25 +0900

    [add]: DefaultContainer.js

---
+++ src/components/templates/DefaultContainer.js
@@ -0,0 +1,24 @@
+/**
+ * すべてのページの基礎となるコンポーネント
+ */
+import styled from "styled-components";
+import { useEffect } from "react";
+
+export const DefaultContainer = ({ children, title }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  // ページタイトルを変更する関数
+  useEffect(() => {
+    document.title = `${title} | Hirotask Portfolio`;
+  }, [title]);
+
+  return <MainWrapper>{children}</MainWrapper>;
+};
+
+const MainWrapper = styled.div`
+  display: flex;
+  border: #478dc0 20px solid;
+  padding: 60px;
+`;
