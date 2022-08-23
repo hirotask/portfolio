05b6c85e283fe054299f844daec62b4a1717a8b6
hirotask <hirotaskhashi@gmail.com>
2022-08-23 21:55:06 +0900

    [update]: Aboutページのレイアウトを調整

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -4,16 +4,14 @@
 import { DefaultContainer } from "../templates/DefaultContainer";
 import { MyIcon } from "../atoms/MyIcon";
 import styled from "styled-components";
-import { useState } from "react";
 
 export const AboutPage = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [aboutme, _] = useState([
+  const aboutme = [
     "名前: 波紫寛斗",
     "所属大学: 岩手県立大学　ソフトウェア情報学部",
     "ニックネーム: hirotask",
     "誕生日: 2001年5月24日",
-  ]);
+  ];
 
   return (
     <DefaultContainer title={"About"}>
@@ -35,5 +33,5 @@ const Wrapper = styled.div`
 
 const Messages = styled.div`
   margin-left: 120px;
-  font-size: 32px;
+  font-size: 28px;
 `;
