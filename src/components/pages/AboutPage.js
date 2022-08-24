4687912203deb9681cc51d67b6a03efbe2d1b29b
hirotask <hirotaskhashi@gmail.com>
2022-08-24 15:14:31 +0900

    [update]: Aboutページに経歴を追加

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -13,15 +13,31 @@ export const AboutPage = () => {
     "誕生日: 2001年5月24日",
   ];
 
+  const career = [
+    "2018/01 Minecraftのプラグイン制作を始める",
+    "2020/03 岩手県立葛巻高等学校　卒業",
+    "2020/04 岩手県立大学　ソフトウェア情報学部　入学",
+    "2020/09 基礎教養入門2にて初めてのチーム開発ジョイン",
+    "2021/08 滝沢イノベーションチャレンジ　１回目の参加",
+    "2021/08 葛巻町プログラミング教室　企画したがコロナの影響で中止",
+    "2021/09 基礎教養入門２にて、○○の開発",
+    "2022/05 Androidアプリ開発開始",
+  ];
+
   return (
     <DefaultContainer title={"About"}>
       <Wrapper>
         <MyIcon size={200} />
-        <Messages>
+        <Aboutme>
           {aboutme.map((msg, key) => (
             <p key={key}>{msg}</p>
           ))}
-        </Messages>
+          <Career>
+            {career.map((msg, key) => (
+              <p key={key}>{msg}</p>
+            ))}
+          </Career>
+        </Aboutme>
       </Wrapper>
     </DefaultContainer>
   );
@@ -31,7 +47,13 @@ const Wrapper = styled.div`
   display: flex;
 `;
 
-const Messages = styled.div`
+const Aboutme = styled.div`
   margin-left: 120px;
   font-size: 28px;
+`;
+
+const Career = styled.div`
+  margin-top: 30px;
+  color: #444444;
+  font-size: 18px;
 `;
