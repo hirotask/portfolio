e22322958d2b9fb005ac21d6b688717a8462c51c
hirotask <hirotaskhashi@gmail.com>
2022-08-24 18:00:40 +0900

    [update]: Issue #1を実装

--- src/components/atoms/CategoryItem.js
+++ src/components/atoms/CategoryItem.js
@@ -14,6 +14,28 @@ const CategoryItemText = styled.div`
   font-size: 18px;
   font-weight: bold;
   border: solid 2px #000000;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+  cursor: pointer;
+
+  &::after {
+    background: rgba(125, 201, 255, 0.6);
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "";
+    width: 100%;
+    transform-origin: left top;
+    transition: 0.3s cubic-bezier(0.45, 0, 0.55, 1);
+    z-index: -1;
+    transform: skewY(-10deg) scale(1, 0);
+    height: 140px; // ボタンの高さによって調整
+  }
+
+  &:hover::after {
+    transform: skewY(-10deg) scale(1, 1);
+  }
 
   & p {
     margin: 0;
