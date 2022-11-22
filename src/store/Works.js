e472fd25d5ed7fc7325154ccc5f6a60f303e4805
hirotask <hirotaskhashi@gmail.com>
2022-11-22 23:37:07 +0900

    [add]: 演劇サークル長経験

--- src/store/Works.js
+++ src/store/Works.js
@@ -1,20 +1,28 @@
 import WorkCategory from "../util/WorkCategory";
 import tech10 from "../assets/img/tech10.jpg";
 import germany from "../assets/img/germany.JPG";
+import act from "../assets/img/act.jpg";
 
 const Works = [
+    {
+        category: WorkCategory.OTHERS,
+        imgSrc: germany,
+        title: "ドイツ研修",
+        description: "くずまき高原国際交流推進協議会主催の欧州視察（ドイツ研修）に参加しました。異文化に触れ、自らの視野が広がった貴重な体験でした。"
+    },
+    {
+        category: WorkCategory.OTHERS,
+        imgSrc: act,
+        title: "演劇サークル長経験",
+        description: "大学在学中は演劇サークルに所属し、サークル長を務めていました。"
+    },
     {
         category: WorkCategory.MOBILE_APPS,
         imgSrc: tech10,
         title: "東北Tech道場10周年イベント",
         description: "『東北Tech道場』というイベントの10周年記念で、渋谷Googleオフィスにて製作中のモバイルアプリに関するプレゼンを行いました。"
     },
-    {
-        category: WorkCategory.OTHERS,
-        imgSrc: germany,
-        title: "ドイツ研修",
-        description: "くずまき高原国際交流推進協議会主催の欧州視察（ドイツ研修）に参加しました。異文化に触れ、自らの視野が広がった貴重な体験でした。"
-    }
+
 ]
 
 export default Works;
