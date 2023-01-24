ea3794a9e7d023c6ed65df746d5d6779fc4e8761
hirotask <hirotaskhashi@gmail.com>
2023-01-24 15:34:57 +0900

    [update]: DefaultContainer.jsのレスポンシブ化

--- src/components/templates/DefaultContainer.js
+++ src/components/templates/DefaultContainer.js
@@ -1,36 +1,32 @@
 /**
  * すべてのページの基礎となるコンポーネント
  */
-import { useEffect } from "react";
-import styled from "styled-components";
+import { useEffect } from 'react';
+import styled from 'styled-components';
 
+import { Header } from '../organisms/Header';
 
+export const DefaultContainer = ({ children, title }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-import { Header } from "../organisms/Header";
+  // ページタイトルを変更する関数
+  useEffect(() => {
+    document.title = `${title} | Hirotask Portfolio`;
+  }, [title]);
 
-
-export const DefaultContainer = ({children, title}) => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-
-    // ページタイトルを変更する関数
-    useEffect(() => {
-        document.title = `${title} | Hirotask Portfolio`;
-    }, [title]);
-
-    return (
-        <MainWrapper>
-            <Header/>
-            {children}
-        </MainWrapper>
-    );
+  return (
+    <MainWrapper>
+      <Header />
+      {children}
+    </MainWrapper>
+  );
 };
 
 const MainWrapper = styled.div`
-  max-width: 1440px;
-  min-height: 90vh;
+  min-height: 95vh;
   border: #478dc0 20px solid;
-  padding: 60px 120px;
-  margin: 40px auto;
+  padding: 4em 5em;
+  margin: 40px 2%;
 `;
